6ef0e4db7d2e1ef402ce495c54d66720d1501ff8
Abed Davarpanah <abed.davarpanah@gmail.com>
2026-04-22 12:59:52 +0200

    Complete get_data step

--- data.py
+++ data.py
@@ -20,7 +20,34 @@ class Olist:
         Its keys should be 'sellers', 'orders', 'order_items' etc...
         Its values should be pandas.DataFrames loaded from csv files
         """
-        pass  # YOUR CODE HERE
+        csv_path = Path("~/.lewagon/olist/data/csv").expanduser()
+        csv_path
+        file_paths = list(csv_path.iterdir())
+        file_paths
+        # Test your code below. We try to load the first csv in the directory
+        import pandas as pd
+        pd.read_csv(file_paths[0]).head()
+
+        file_names = ['olist_customers_dataset.csv',
+              'product_category_name_translation.csv',
+              'olist_orders_dataset.csv',
+              'olist_order_items_dataset.csv',
+              'olist_geolocation_dataset.csv',
+              'olist_order_reviews_dataset.csv',
+              'olist_order_payments_dataset.csv',
+              'olist_products_dataset.csv',
+              'olist_sellers_dataset.csv']
+
+        key_names=[]
+        for names in file_names:
+            key_names.append(names.replace('_dataset.csv','').replace('olist_','').replace('.csv',''))
+
+        data = {}
+        for key, path in zip(key_names, file_paths):
+            data[key] = pd.read_csv(path)
+
+        return data
+
 
     def ping(self):
         """
